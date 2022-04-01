1104b43710979723cdb1bbc395ffa12768ec96cf
chimeddorj <chimeddorj86@yahoo.com>
2022-04-01 21:45:18 +0900

    hicheel.js pc

--- hicheel.js
+++ hicheel.js
@@ -1,45 +1,127 @@
- const superMan = {
-		name: "chimeddorj",
-    age : 25,
-}
-console.log(superMan.name);
-console.log(superMan['age']);
-superMan.school = "KonkukU";
-console.log(superMan)
-delete superMan.age;
-console.log(superMan);
+// const name = prompt("zahialgaa oruulna uu","2022-03-");
+// alert(`zahialga batalgaajlaa ${name}`);
 
-const add = (name, age)=>{
-	return{
-		name : name,
-    age: age,
-	}
-}
+// const isAdult = confirm("nasand hvrsen vv? ");
+// console.log(isAdult)
 
-const result = add("jihyeon", 24);
-console.log(result);
+// let n = 10;
+// daraah hoyr ni adilhan
+// n = n+5; n+=5;
+// n+=5;
+// console.log(n);
 
-const isAdult = (user)=>{
-		if(!('age' in user) || user.age < 20 ){
-				return false;
-		}
-				return true;
-}
+// let i = 1;
+// while(i<=10){
+//     console.log(i);
+//     i++;
+// }
+// for (let i = 0; i<10;i++){
+//     if(i%2){
+//         console.log(i)
+//         // continue
+//     }
+//     // console.log(i)
+// }
 
-const mike = {
-	name: "mike",
-  age : 25
-};
 
-const lisa = {
-	name: "lisa"
-};
+// const pass = prompt('pass: ')
+// const user = prompt('user: ')
+// switch(pass && user){
+//     case (p && u):
+//         console.log(logged);
+//         break
+// }
 
-console.log(isAdult(mike))
 
-for(i in mike){
-	console.log(mike[i])
-}
+// function helloWorld(name = "friend"){
+//     let a = `hello ${name}`;
+//     console.log(a);
+// }
+
+// helloWorld();
+// helloWorld("chimeddorj");
+
+//************** 선언문 ************
+
+// function add(number1, number2){
+//     return number1+number2;
+// }
+// const sum = add(2,4)
+// console.log(sum);
+
+// *********************** 함수 표현식 ********************
+// let add = function(number1, number2){
+//     return number1+number2;
+// }
+// const sum = add(2,4)
+// console.log(sum);
+
+
+// ************** 화살표 함수 ******************
+// let add = (numb1, numb2)=>{
+//     return numb1 + numb2;
+// }
+// let result = add(20,40);
+// console.log(result);
+
+// let add = (numb1, numb2) => numb1+numb2;
+// let result = add(24034,3492);
+// console.log(result);
+
+// const name = "jaraa";
+// const age = 25;
+// const arrObject = {
+//     name,
+//     age,
+//     school: "konkuk"
+// }
+// console.log('school' in arrObject);
+
+
+//  const superMan = {
+// 		name: "chimeddorj",
+//     age : 25,
+// }
+// console.log(superMan.name);
+// console.log(superMan['age']);
+// superMan.school = "KonkukU";
+// console.log(superMan)
+// delete superMan.age;
+// console.log(superMan);
+
+// const add = (name, age)=>{
+// 	return{
+// 		name : name,
+//     age: age,
+// 	}
+// }
+
+// const result = add("jihyeon", 24);
+// console.log(result);
+
+// const isAdult = (user)=>{
+// 		if(!('age' in user) || user.age < 20 ){
+// 				return false;
+// 		}
+// 				return true;
+// }
+
+// const mike = {
+// 	name: "mike",
+//   age : 25
+// };
+
+// const lisa = {
+// 	name: "lisa"
+// };
+
+// console.log(isAdult(mike))
+
+// for(i in mike){
+// 	console.log(mike[i])
+// }
+
+
 
 
 
